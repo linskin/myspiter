cc8916fd478276bd06dba1a48514d1108da47318
linskin <1360293790@qq.com>
2024-04-02 21:17:38 +0800

    修正renamer中的已知错误

--- myproject/output/pdf_output/renamer/renamer.py
+++ myproject/output/pdf_output/renamer/renamer.py
@@ -9,6 +9,8 @@ def extract_article_title_from_pdf(pdf_path):
         tmp_title = text.split('\n')[4]
         if tmp_title == '∗':
             tmp_title = text.split('\n')[3]
+        elif tmp_title == '*':
+            tmp_title = text.split('\n')[3]
         end_title = ""
         for character in tmp_title:
             if character in '\\/:*?"<>|':
@@ -21,7 +23,7 @@ def extract_article_title_from_pdf(pdf_path):
 
 if __name__ == "__main__":
     # 指定 PDF 文件夹路径
-    pdf_folder = '../软件学报2024'
+    pdf_folder = '../软件学报202205'
     # 遍历 PDF 文件夹中的所有文件
     for filename in os.listdir(pdf_folder):
         # 拼接 PDF 文件的完整路径
@@ -31,8 +33,8 @@ if __name__ == "__main__":
             # 提取文章标题
             article_title = extract_article_title_from_pdf(pdf_path)
             print(article_title)
-            # # 修改文件名为文章标题
-            # new_filename = f"{article_title}.pdf"
-            # # 重命名文件
-            # os.rename(pdf_path, os.path.join(pdf_folder, new_filename))
-            # print(f"Renamed {pdf_path} to {new_filename}")
+            # 修改文件名为文章标题
+            new_filename = f"{article_title}.pdf"
+            # 重命名文件
+            os.rename(pdf_path, os.path.join(pdf_folder, new_filename))
+            print(f"Renamed {filename} to {new_filename}")
---
+++ myproject/output/pdf_output/renamer/tester.py
@@ -0,0 +1,29 @@
+import os
+import pdfplumber
+
+
+# 目前只试过软件学报的文献
+def extract_article_title_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = pdf.pages[0].extract_text()
+        tmp_title = text.split('\n')[4]
+        if tmp_title == '∗' or "*":
+            tmp_title = text.split('\n')[3]
+        end_title = ""
+        for character in tmp_title:
+            if character in '\\/:*?"<>|':
+                continue
+            else:
+                end_title += character
+
+    return end_title
+
+
+if __name__ == "__main__":
+    pdf_path = '.pdf'
+    with pdfplumber.open(pdf_path) as pdf:
+        text = pdf.pages[0].extract_text()
+    title = text.split('\n')[4]
+    print(title)
+    title2 = extract_article_title_from_pdf(pdf_path)
+    print(title2)
